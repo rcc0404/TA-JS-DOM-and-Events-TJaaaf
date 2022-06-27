b5fd227d32049eee23e885ce6915f05859edac2c
rcc0404 <rahulchaudhar3@gmail.com>
2022-06-27 21:57:56 +0530

    Event2

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,1 +1,21 @@
-.
+let firstBoxes = document.querySelectorAll(".first li");
+let secondBoxe = document.querySelectorAll(".second");
+
+firstBoxes.forEach((box, index) => {
+    box.addEventListener("click", (event) => {
+        event.target.innerText = index + 1;
+
+        setTimeout(() => {
+            event.target.innerText = "";
+        }, 5000);
+    });
+});
+
+secondBoxe.addEventListener("click", (event) => {
+    let text = event.target.dataset.text;
+   event.target.innerText = text;
+
+        setTimeout(() => {
+            event.target.innerText = "";
+        }, 5000);
+    });
